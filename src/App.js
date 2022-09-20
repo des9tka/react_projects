b158790abba250c93fbeb9ae20ae94861e52a402
des9tka <des9tka2005@ukr.net>
2022-09-20 09:28:09 +0300

    com

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import GetUsers from "./data/Users";
-import {users} from "./data/Users";
+
+
 
 function App() {
   return (
       <div>
-          <GetUsers/>
+
       </div>
   );
 }
